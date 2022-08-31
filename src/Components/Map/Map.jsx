5ab8a2409aeb339a8ec38a56fdde67be7bd6a152
Jonathan Tri <jonathantric@gmail.com>
2022-08-31 19:22:48 +0700

    fix goggle map

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import GoogleMapReact from "google-map-react";
+// import { Map, GoogleApiWrapper } from "google-maps-react";
 
 const mapStyles = {
   width: "90%",
@@ -7,22 +8,38 @@ const mapStyles = {
   margin: "0 auto",
 };
 
+const defaultProps = {
+  center: {
+    lat: -6.247919053750098,
+    lng: 106.7463764125819,
+  },
+  zoom: 11,
+};
 export class MapContainer extends Component {
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={18}
-        style={mapStyles}
-        initialCenter={{
-          lat: -6.247919053750098,
-          lng: 106.7463764125819,
-        }}
-      />
+      <>
+        {/* <Map
+          google={this.props.google}
+          zoom={18}
+          style={mapStyles}
+          initialCenter={{
+            lat: -6.247919053750098,
+            lng: 106.7463764125819,
+          }}
+        /> */}
+
+        <GoogleMapReact
+          style={mapStyles}
+          bootstrapURLKeys={{ key: "AIzaSyDs8RNEcTcj3DgX50hoFkRVGUV3stHfFe8" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        ></GoogleMapReact>
+      </>
     );
   }
 }
 
-MapContainer = GoogleApiWrapper({
-  apiKey: "AIzaSyDs8RNEcTcj3DgX50hoFkRVGUV3stHfFe8",
-})(MapContainer);
+// MapContainer = GoogleApiWrapper({
+//   apiKey: "AIzaSyDs8RNEcTcj3DgX50hoFkRVGUV3stHfFe8",
+// })(MapContainer);
